d07b582fec7a87f68a713fe4cdc72015a2631c4a
martin-martin <breuss.martin@gmail.com>
2017-12-15 18:35:53 -0600

    add keys

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -15,7 +15,7 @@ class SearchBooks extends Component {
         ],
         "publisher": "No Starch Press",
         "publishedDate": "2012",
-        "description": "You've experienced the shiny, point-and-click surface of your Linux computer\u2014now dive below and explore its depths with the power of the command line. The Linux Command Line takes you from your very first terminal keystrokes to writing full programs in Bash, the most popular Linux shell. Along the way you'll learn the timeless skills handed down by generations of gray-bearded, mouse-shunning gurus: file navigation, environment configuration, command chaining pattern matching with regular expressions, and more. In addition to that practical knowledge, author William Shotts reveals the philosophy behind these tools and the rich heritage that your desktop Linux machine has inherited from Unix supercomputers of yore. As you make your way through the book's short, easily-digestible chapters, you'll learn how to: * Create and delete files, directories, and symlinks * Administer your system, including networking, package installation, and process management * Use standard input and output, redirection, and pipelines * Edit files with Vi, the world\u2019s most popular text editor * Write shell scripts to automate common or boring tasks * Slice and dice text files with cut, paste, grep, patch, and sed Once you overcome your initial \"shell shock,\" you'll find that the command line is a natural and expressive way to communicate with your computer. Just don't be surprised if your mouse starts to gather dust. A featured resource in the Linux Foundation's \'Evolution of a SysAdmin\'",
+        "description": "You've experienced the shiny",
         "industryIdentifiers": [
           {
             "type": "ISBN_13",
@@ -45,13 +45,13 @@ class SearchBooks extends Component {
           "containsImageBubbles": false
         },
         "imageLinks": {
-          "smallThumbnail": "http:\/\/books.google.com\/books\/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
-          "thumbnail": "http:\/\/books.google.com\/books\/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+          "smallThumbnail": "http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+          "thumbnail": "http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
         },
         "language": "en",
-        "previewLink": "http:\/\/books.google.com\/books?id=nggnmAEACAAJ&dq=linux&hl=&cd=3&source=gbs_api",
-        "infoLink": "https:\/\/play.google.com\/store\/books\/details?id=nggnmAEACAAJ&source=gbs_api",
-        "canonicalVolumeLink": "https:\/\/market.android.com\/details?id=book-nggnmAEACAAJ",
+        "previewLink": "http://books.google.com/books?id=nggnmAEACAAJ&dq=linux&hl=&cd=3&source=gbs_api",
+        "infoLink": "https://play.google.com/store/books/details?id=nggnmAEACAAJ&source=gbs_api",
+        "canonicalVolumeLink": "https://market.android.com/details?id=book-nggnmAEACAAJ",
         "id": "nggnmAEACAAJ",
         "shelf": "currentlyReading"
       }
@@ -101,8 +101,8 @@ class SearchBooks extends Component {
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            {this.state.books.map((book) => (
-              <li>
+            {this.state.books.map((book, i) => (
+              <li key={i}>
                 <div className="book">
                   <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
@@ -117,8 +117,8 @@ class SearchBooks extends Component {
                     </div>
                   </div>
                   <div className="book-title">{book.title}</div>
-                  {book.authors.map((author) => (
-                    <div className="book-authors">{author}</div>
+                  {book.authors.map((author, j) => (
+                    <div className="book-authors" key={j}>{author}</div>
                   ))}
                 </div>
               </li>
