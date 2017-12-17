126441dec31234f058ed6ace30c85f5997819032
martin-martin <breuss.martin@gmail.com>
2017-12-17 10:32:24 -0600

    add book component

---
+++ src/Book.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+
+class Book extends Component {
+
+  render() {
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover"
+                style={{ width: 128,
+                        height: 193,
+                        backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}>
+          </div>
+          <div className="book-shelf-changer">
+            <select>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">Goodwill</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{this.props.book.title}</div>
+        {this.props.book.authors.map((author, i) => (
+          <div className="book-authors" key={i}>{author}</div>
+        ))}
+      </div>
+    )
+  }
+}
+
+export default Book
