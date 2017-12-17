f8c07915651aa32180592f4203e9ea3f2816d75f
martin-martin <breuss.martin@gmail.com>
2017-12-17 12:19:11 -0600

    show selected shelf in dropdown

--- src/Book.js
+++ src/Book.js
@@ -13,7 +13,9 @@ class Book extends Component {
                         backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}>
           </div>
           <div className="book-shelf-changer">
-            <select>
+            <select
+              value={this.props.book.shelf}
+            >
               <option value="none" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
